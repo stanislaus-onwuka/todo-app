9072b1f0923daca157e5b1889f2a430be86bddc5
stanislaus-onwuka <kachi2505@yahoo.com>
2021-07-11 09:07:49 +0000

    First commit

--- App.js
+++ App.js
@@ -1,11 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Pressable, Alert } from 'react-native';
 
 export default function App() {
+  const createThreeButtonAlert = () =>
+  Alert.alert(
+    "Alert Title",
+    "My Alert Msg",
+    [
+      {
+        text: "Ask me later",
+        onPress: () => console.log("Ask me later pressed")
+      },
+      {
+        text: "Cancel",
+        onPress: () => console.log("Cancel Pressed"),
+        style: "cancel"
+      },
+      { text: "OK", onPress: () => console.log("OK Pressed") }
+    ],
+    { cancelable: false }
+    );
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Open up App.js to start working</Text>
+      <Pressable onPress={createThreeButtonAlert}>
+        <Text>Hi</Text>
+      </Pressable>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +36,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FF6F28',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color:"#fff",
+  }
 });
